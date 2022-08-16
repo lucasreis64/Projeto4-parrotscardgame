d05450984e2f9327f88c31db1030b8f2c40fd9f8
Lucas Reis <lucasreis64@gmail.com>
2022-08-16 17:33:40 -0300

    Baralho pronto

--- javascript/script.js
+++ javascript/script.js
@@ -2,33 +2,38 @@ let escolha = 10,
     baralho = [1, 2, 3, 4, 5, 6, 7],
     posicao = [],
     final = false;
-while (final===false) {
-    iniciarJogo();
-    cartear();
-}
+
+const superior = document.querySelector('.fileirasuperior');
+const inferior = document.querySelector('.fileirainferior');
+
+iniciarJogo();
+cartear();
+
 function iniciarJogo() {
     do {
         if ((escolha > 14 || escolha < 4 || escolha % 2 !== 0) || (escolha > 14 || escolha < 4 && escolha % 2 !== 0)) {
             alert("Valor inválido, tente novamente!");
-        };
+        }
         escolha = prompt("Escolha o número de cartas (somente quantidade par de 4 à 14):");
-    } while ((escolha > 14 || escolha < 4 || escolha % 2 !== 0) || (escolha > 14 || escolha < 4 && escolha % 2 !== 0))
+    } while ((escolha > 14 || escolha < 4 || escolha % 2 !== 0) || (escolha > 14 || escolha < 4 && escolha % 2 !== 0)) console.log('17')
 }
 
 function cartear() {
     let cont2 = 0;
     const tamanho = escolha;
-    const superior = document.querySelector('.fileirasuperior');
-    const inferior = document.querySelector('.fileirainferior');
     baralho.sort(comparador);
+
     for (let cont = 0; cont < tamanho; cont += 2) {
         posicao[cont] = criarcarta(baralho[cont2]);
         posicao[cont + 1] = criarcarta(baralho[cont2]);
         cont2++;
     }
+
     posicao.sort(comparador);
+
     for (let cont = 0; cont < tamanho; cont++) {
         if (cont < tamanho / 2) {
+            console.log(',37', posicao[cont])
             posicao[cont] = superior.appendChild(posicao[cont]);
         }
         if (cont >= tamanho / 2) {
