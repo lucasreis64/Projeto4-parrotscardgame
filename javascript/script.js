4694f99f3475cb9822fe7f5109e5afa6bfe9bbc3
Lucas Reis <lucasreis64@gmail.com>
2022-08-16 16:28:58 -0300

    inicio

--- javascript/script.js
+++ javascript/script.js
@@ -2,23 +2,24 @@ let escolha = 10,
     baralho = [1, 2, 3, 4, 5, 6, 7],
     posicao = [],
     final = false;
-
+while (final===false) {
     iniciarJogo();
-
+    cartear();
+}
 function iniciarJogo() {
     do {
         if ((escolha > 14 || escolha < 4 || escolha % 2 !== 0) || (escolha > 14 || escolha < 4 && escolha % 2 !== 0)) {
             alert("Valor inválido, tente novamente!");
         };
         escolha = prompt("Escolha o número de cartas (somente quantidade par de 4 à 14):");
-    } while ((escolha > 14 || escolha < 4 || escolha % 2 !== 0) || (escolha > 14 || escolha < 4 && escolha % 2 !== 0));
+    } while ((escolha > 14 || escolha < 4 || escolha % 2 !== 0) || (escolha > 14 || escolha < 4 && escolha % 2 !== 0))
 }
 
 function cartear() {
     let cont2 = 0;
     const tamanho = escolha;
     const superior = document.querySelector('.fileirasuperior');
-    const inferior = document.querySelector('.fileirainferior')
+    const inferior = document.querySelector('.fileirainferior');
     baralho.sort(comparador);
     for (let cont = 0; cont < tamanho; cont += 2) {
         posicao[cont] = criarcarta(baralho[cont2]);
